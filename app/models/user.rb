bed5cf81eca25928586f73b9b3c12640e0e91273
Alex Stone <alexstone@berkeley.edu>
2012-04-08 16:51:16 -0700

    User Model Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ActiveRecord::Base
+  has_many :sessions, :dependent => :destroy
+
+  attr_accessible :name, :email
+  
+  def self.find_from_hash(hash)
+    User.find_by_name_and_email(hash['info']['name'], hash['info']['email'])
+  end
+
+  def self.create_from_hash(hash)
+    create(:name => hash['info']['name'], :email => hash['info']['email'])
+  end
+
+  def is_admin?
+    x = []
+    x << self.role
+    (x & ROLES)[0] == ROLES[0]
+  end
+
+  def self.get_roles
+    return ROLES
+  end
+
+  private
+    ROLES = %w{admin clerk producer guest}
+
 end
