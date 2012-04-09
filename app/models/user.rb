8d366c134424b193e641ee224e205199bb6ff0b6
Alex Stone <alexstone@berkeley.edu>
2012-04-08 17:34:33 -0700

    Removed Unneccessary User Model Methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,9 @@
 class User < ActiveRecord::Base
   has_many :sessions, :dependent => :destroy
+  has_many :transactions
 
   attr_accessible :name, :email
   
-  def self.find_from_hash(hash)
-    User.find_by_name_and_email(hash['info']['name'], hash['info']['email'])
-  end
-
-  def self.create_from_hash(hash)
-    create(:name => hash['info']['name'], :email => hash['info']['email'])
-  end
-
   def is_admin?
     x = []
     x << self.role
