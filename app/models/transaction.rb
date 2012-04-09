d0795677ba874e7acfa5a00d38854a59a342114f
Alex Stone <alexstone@berkeley.edu>
2012-04-08 17:16:15 -0700

    Transaction Model Updated

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,14 @@
 class Transaction < ActiveRecord::Base
+  belongs_to :user
+
+  attr_accessible :producer, :code, :center, :date, :lot_number, :sack_number, :gross_weight, :tare, :net_weight, :comments, :delivery_form, :price, :quality, :classes, :s_f, :bitten, :bare, :green, :drill, :mold, :gqmd, :total, :received_by, :user_id
+
+  def self.get_centers
+    ['COMPROCOOM', 'San Jose de los Auctions', 'Rancho Grande', 'Wibuse', 'The Chocolata']
+  end
+  
+  def self.get_delivery_forms
+    ['Delivery', 'Purchase']
+  end
+
 end
