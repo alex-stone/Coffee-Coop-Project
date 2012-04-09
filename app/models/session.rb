0bc481bf317906c9b43fb1c3efbd5335450c7510
Alex Stone <alexstone@berkeley.edu>
2012-04-08 18:52:13 -0700

    Session Model - Added method to standardized returned omniauth hash

--- app/models/session.rb
+++ app/models/session.rb
@@ -9,6 +9,30 @@ class Session < ActiveRecord::Base
     find_by_provider_and_uid(hash['provider'], hash['uid'])
   end
 
+  def self.create_authhash(hash, service)
+    new_hash = Hash.new
+  
+    if service == 'facebook'
+      hash['extra']['user_hash']['email'] ? new_hash[:email] =  hash['extra']['user_hash']['email'] : new_hash[:email] = ''
+      hash['extra']['user_hash']['name'] ? new_hash[:name] =  hash['extra']['user_hash']['name'] : new_hash[:name] = ''
+      hash['extra']['user_hash']['id'] ?  new_hash[:uid] =  hash['extra']['user_hash']['id'].to_s : new_hash[:uid] = ''
+      hash['provider'] ? new_hash[:provider] = hash['provider'] : new_hash[:provider] = ''
+    elsif service == 'github'
+      hash['info']['email'] ? new_hash[:email] =  hash['info']['email'] : new_hash[:email] = ''
+      hash['info']['name'] ? new_hash[:name] =  hash['info']['name'] : new_hash[:name] = ''
+      hash['extra']['user_hash']['id'] ? new_hash[:uid] =  hash['extra']['user_hash']['id'].to_s : new_hash[:uid] = ''
+      hash['provider'] ? new_hash[:provider] =  hash['provider'] : new_hash[:provider] = ''  
+    elsif ['google', 'yahoo', 'twitter', 'myopenid', 'open_id'].index(service_route) != nil
+      hash['info']['email'] ? new_hash[:email] =  hash['info']['email'] : new_hash[:email] = ''
+      hash['info']['name'] ? new_hash[:name] =  hash['info']['name'] : new_hash[:name] = ''
+      hash['uid'] ? new_hash[:uid] = hash['uid'].to_s : new_hash[:uid] = ''
+      hash['provider'] ? new_hash[:provider] = hash['provider'] : new_hash[:provider] = ''
+    else
+      return nil
+    end
+    return new_hash
+  end
+
   def self.create_from_hash(hash, user=nil)
     user ||= User.create_from_hash(hash)
     Session.create(:user_id => user.id, :uid => hash['uid'], :provider => hash['provider'])
