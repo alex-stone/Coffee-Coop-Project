6a566268905a0822e66aa7e8e6b741c9c51a8f23
Alex Stone <alexstone@berkeley.edu>
2012-05-03 00:04:21 -0700

    Changed Navbar min-width to work with Spanish longer words

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -2,7 +2,6 @@ class ProducersController < ApplicationController
   def index 
     @producers = User.find_all_by_role('producer')
     @is_admin = user_is_admin?
-<<<<<<< HEAD
     if params[:center_id]
       @has_center = true
       @center = Center.find_by_id(params[:center_id])
@@ -10,7 +9,5 @@ class ProducersController < ApplicationController
       @has_center = false
     end
 
-=======
->>>>>>> 915fb89b87ea5f829ed6192242058ac6bead2dcc
   end 
 end
