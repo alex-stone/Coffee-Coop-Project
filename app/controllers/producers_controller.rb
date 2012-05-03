51b0b201bef953c247f388ba0ea24ab6cc4a1afa
Alex Stone <alexstone@berkeley.edu>
2012-05-03 02:26:54 -0700

    Fixed Producers Filtering by Center

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,12 +1,13 @@
 class ProducersController < ApplicationController
   def index 
-    @producers = User.find_all_by_role('producer')
     @is_admin = user_is_admin?
     if params[:center_id]
       @has_center = true
       @center = Center.find_by_id(params[:center_id])
+      @producers = User.find(:all, :conditions => {:role => 'producer', :center => @center.name})
     else
       @has_center = false
+      @producers = User.find_all_by_role('producer')
     end
 
   end 
